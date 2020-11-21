7e6a7941de1446ac5e5f45d53b9213ac9d1da030
Filippo <Dell@DESKTOP-2751OC0>
2020-11-21 17:28:21 +0100

    Aggiungere i file di progetto.

---
+++ RaccoltaPunti/Punto1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RaccoltaPunti
+{
+    public class Punto1
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Punto1 (double x , double y )
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ RaccoltaPunti/Punto2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaccoltaPunti
+{
+    class Punto2 : Punto1
+    {
+        public double z { get; set; }
+        public Punto2(double x, double y, double z) : base(x, y)
+        {
+            this.z = z;
+        }
+    }
+}
